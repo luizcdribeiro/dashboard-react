9e2d3a6da42e1990c37cd47f3099b7d213576bfe
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2021-10-08 08:59:42 -0300

    instalacao do axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,12 +5,12 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from '../../components/Header';
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from '../../components/Sidebar'
+import { api } from '../../services/api';
 
 export default function UserList() {
 
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
+  const { data, isLoading, error, isFetching } = useQuery('users', async () => {
+    const { data } = await api.get<any>('users');
 
     const users = data.users.map(user => {
       return {
@@ -43,7 +43,10 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal" >Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+            </Heading>
 
             <Link passHref href="/users/create">
               <Button
