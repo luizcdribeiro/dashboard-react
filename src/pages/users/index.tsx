5be470d39b523da7006809c89252a97e18ad3272
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2021-10-08 01:15:19 -0300

    configurando react query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,27 +1,40 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import Link from 'next/link'
+import { useQuery } from 'react-query'
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from '../../components/Header';
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from '../../components/Sidebar'
-import Link from 'next/link'
-import {useEffect} from 'react'
 
 export default function UserList() {
+
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        })
+      }
+    })
+
+    return users;
+
+  })
 
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
 
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }, [])
-
-  
   return(
-
     <Box >
       <Header />
 
@@ -46,122 +59,75 @@ export default function UserList() {
             </Link>
           </Flex> 
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px="6" color="gray.300" width="8" >
-                  <Checkbox colorScheme="pink" />
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+            
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px="6" color="gray.300" width="8" >
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>
+                      Usuário 
+                    </Th>
+                    {isWideVersion &&
+                      <Th>
+                        Data de cadastro 
+                      </Th>
+                    }
 
-                </Th>
-                <Th>
-                  Usuário 
-                </Th>
-                {isWideVersion &&
-                  <Th>
-                    Data de cadastro 
-                  </Th>
-                }
+                    <Th>
+                      {isWideVersion ? 'Editar' : ''}
+                    </Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={['4', '4', '6']}>
+                          <Checkbox colorScheme="pink" />
 
-                <Th>
-                  {isWideVersion ? 'Editar' : ''}
-                </Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text color="gray.300" fontSize="sm">{user.email}</Text>
 
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Luiz Carlos Ribeiro</Text>
-                    <Text color="gray.300" fontSize="sm">luizjunior.dantas@gmail.com</Text>
+                          </Box>
+                        </Td>
+                        { isWideVersion && <Td> {user.createdAt} </Td> }
+                        <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                        >
 
-                  </Box>
-                </Td>
-                {isWideVersion && 
-                  <Td>
-                    12 de Agosto de 2021
-                  </Td>
-                }
-                <Td>
-                <Button
-                  as="a"
-                  size="sm"
-                  fontSize="sm"
-                  colorScheme="purple"
-                  leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                >
+                          { isWideVersion ? 'Editar' : '' }
+                        </Button>
+                        </Td>
+                      </Tr>
+                      
+                    )
+                  })}
+                </Tbody>
+              </Table>
 
-                  {isWideVersion ? 'Editar' : ''}
-                </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Luiz Carlos Ribeiro</Text>
-                    <Text color="gray.300" fontSize="sm">luizjunior.dantas@gmail.com</Text>
-
-                  </Box>
-                </Td>
-                {isWideVersion && 
-                  <Td>
-                    12 de Agosto de 2021
-                  </Td>
-                }
-                <Td>
-                <Button
-                  as="a"
-                  size="sm"
-                  fontSize="sm"
-                  colorScheme="purple"
-                  leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                >
-
-                  {isWideVersion ? 'Editar' : ''}
-                </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Luiz Carlos Ribeiro</Text>
-                    <Text color="gray.300" fontSize="sm">luizjunior.dantas@gmail.com</Text>
-
-                  </Box>
-                </Td>
-                {isWideVersion && 
-                  <Td>
-                    12 de Agosto de 2021
-                  </Td>
-                }
-                <Td>
-                <Button
-                  as="a"
-                  size="sm"
-                  fontSize="sm"
-                  colorScheme="purple"
-                  leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                >
-
-                  {isWideVersion ? 'Editar' : ''}
-                </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
 
       </Flex>
